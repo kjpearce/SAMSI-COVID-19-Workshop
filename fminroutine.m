64249faf5ea7e9a4d8a06bbaeda8a29bd3b66602
Kate Pearce <74689733+kjpearce@users.noreply.github.com>
2021-05-07 16:35:29 -0400

    Delete fminroutine.m

--- fminroutine.m
+++
@@ -1,9 +0,0 @@
-function [Kopt, fval] = fminroutine(hvec, tvec, N, K0, qbar, params, lb, ub)
-    
-    options = optimset('Display','iter','TolFun', 1e-12, 'TolX', 1e-8);
-    
-    optfun = @(K) objfun(hvec, tvec, N, K, qbar, params);
-    
-    [Kopt, fval] = fmincon(optfun, K0, [], [], [], [], lb, ub, [], options);
-
-end
